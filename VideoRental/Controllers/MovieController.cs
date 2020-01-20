dcfb0f9f6f13dd9a5270ef5582096bdd432e3357
MahdiDroid <mehdi.mhmvd@gmail.com>
2020-01-20 13:09:17 +0300

    view movie added

---
+++ VideoRental/Controllers/MovieController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VideoRental.Controllers
+{
+    public class MovieController : Controller
+    {
+        private VRDbContext  _Context;
+
+        public MovieController()
+        {
+            _Context = new VRDbContext();
+        }
+
+        // GET: Movie
+        public ActionResult Index()
+        {
+            var movies = _Context.Movies.ToList();
+            return View(movies);
+            //return Content("Hallo everyone");
+        }
+        public ActionResult Details(int id)
+        {
+            var singleMovie = _Context.Movies.SingleOrDefault(m => m.Id == id);
+            return View(singleMovie);
+        }
+    }
+}
