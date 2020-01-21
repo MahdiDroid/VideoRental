c10d13cc5aa80ec04711b4b85b3c50db52d54a20
MahdiDroid <mehdi.mhmvd@gmail.com>
2020-01-21 19:59:14 +0300

    create customer refactor to Dto

--- VideoRental/Controllers/Api/CustomersController.cs
+++ VideoRental/Controllers/Api/CustomersController.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using VideoRental.Dtos;
 using VideoRental.Models;
 
 namespace VideoRental.Controllers.Api
@@ -17,31 +19,33 @@ namespace VideoRental.Controllers.Api
         }
 
         //Get /api/customers
-        public IEnumerable<Customer> GetCustomers()
+        public IEnumerable<CustomerDto> GetCustomers()
         {
-            return _context.Customers.ToList();
+            return _context.Customers.ToList().Select(Mapper.Map<Customer,CustomerDto>);
         }
         //Get /api/customers/id
-        public Customer GetCstomer(int id)
+        public CustomerDto GetCstomer(int id)
         {
             var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
             if (customer == null)
             {
                 throw new HttpResponseException(HttpStatusCode.NotFound);
             }
-            return customer;
+            return Mapper.Map<Customer,CustomerDto> (customer);
         }
         //post /api/customers
         [HttpPost]
-        public Customer CreateCustomer(Customer customer)
+        public CustomerDto CreateCustomer(CustomerDto customerDto)
         {
-/*            if (!ModelState.IsValid)
-            {
-                throw new HttpResponseException(HttpStatusCode.BadRequest);
-            }*/
+            /*            if (!ModelState.IsValid)
+                        {
+                            throw new HttpResponseException(HttpStatusCode.BadRequest);
+                        }*/
+            var customer = Mapper.Map<CustomerDto, Customer>(customerDto);
             _context.Customers.Add(customer);
             _context.SaveChanges();
-            return customer;
+            customerDto.Id = customer.Id;
+            return customerDto;
         }
         [HttpPut]
          public void UpdateCustomer(int id,Customer customer)
